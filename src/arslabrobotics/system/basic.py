c473e79b75ccf73784c2412a8fa68aedde12c1dc
Corrado Santoro <santoro@dmi.unict.it>
2026-08-22 19:42:38 +0200

    chore(system): updated documentation

--- src/arslabrobotics/system/basic.py
+++ src/arslabrobotics/system/basic.py
@@ -13,19 +13,23 @@ import numpy as np
 
 class Proportional:
 
-    def __init__(self, _kp):
+    def __init__(self, _kp : float):
         """
         Defines proportional block
+
         :param _kp: The constant to be multiplied by the input
+
         """
         self.kp = _kp
 
-    def evaluate(self, delta_t, _input):
+    def evaluate(self, delta_t : float, _input : float) -> None:
         """
         Computes the output of proportional block
+
         :param _input: The input
         :param delta_t: The sampling time
         :return: The input times the constant
+
         """
         return _input * self.kp
 
@@ -38,12 +42,14 @@ class Derivator:
         """
         self.prev_input = 0
 
-    def evaluate(self, delta_t, _input):
+    def evaluate(self, delta_t : float, _input : float) -> float:
         """
         Computes the output of the derivator block
+
         :param _input: The input
         :param delta_t: The sampling time
         :return: The input derivated
+
         """
         out = (_input - self.prev_input) / delta_t
         self.prev_input = _input
@@ -58,12 +64,14 @@ class Integrator:
         """
         self.prev_output = 0
 
-    def evaluate(self, delta_t, _input):
+    def evaluate(self, delta_t : float, _input) -> float:
         """
         Computes the output of the integrator
+
         :param _input: The input
         :param delta_t: The sampling time
         :return: The integrated data
+
         """
         out = self.prev_output + _input * delta_t
         self.prev_output = out
@@ -74,12 +82,15 @@ class LinearDynamicSystem:
 
     def __init__(self, A, B, C):
         """
-        Defines a linear system of type:
+        Defines a linear system of type::
+
         x' = A x + B u
         y = C x
+
         :param A: The state matrix
         :param B: The input matrix
         :param C: The output matrix
+
         """
         self.A = np.array(A)
         self.B = np.array(B)
@@ -87,13 +98,15 @@ class LinearDynamicSystem:
         self.order = len(A)
         self.x = np.array( [0] *  self.order)
 
-    def evaluate(self, delta_t, _input):
+    def evaluate(self, delta_t : float, _input : float):
         """
         Evaluates the output of the linear system given the input. Evaluation is made
         by using the Euler approximation.
+
         :param delta_t: The sampling time
         :param _input: The input
         :returns: The output
+
         """
         self.x = (self.A * delta_t + np.eye(self.order)) @ self.x + self.B * delta_t * _input
         output = self.C @ self.x
